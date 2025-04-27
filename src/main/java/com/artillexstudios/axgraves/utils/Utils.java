847e381022c54bc8fb805562a01a37ba4abe4dba
slooshied <thedabcorner@outlook.com>
2025-04-26 22:44:48 -0500

    Add feature for custom skull textures

--- src/main/java/com/artillexstudios/axgraves/utils/Utils.java
+++ src/main/java/com/artillexstudios/axgraves/utils/Utils.java
@@ -1,10 +1,23 @@
 package com.artillexstudios.axgraves.utils;
 
+import com.google.gson.Gson;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonSyntaxException;
+import org.bukkit.Bukkit;
 import org.bukkit.Material;
 import org.bukkit.OfflinePlayer;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.SkullMeta;
+import org.bukkit.profile.PlayerProfile;
+import org.bukkit.profile.PlayerTextures;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.net.URI;
+import java.net.URL;
+import java.util.Base64;
+import java.util.UUID;
+import java.util.logging.Level;
 
 public class Utils {
 
@@ -15,7 +28,50 @@ public class Utils {
             skullMeta.setOwningPlayer(player);
             it.setItemMeta(skullMeta);
         }
-
         return it;
+    }
+
+
+    @Nullable
+    public static ItemStack getBase64Skull(@NotNull String b64Texture) {
+        try {
+            JsonObject jsonObject = new Gson().fromJson(new String(Base64.getDecoder().decode(b64Texture)), JsonObject.class);
+
+            if (!jsonObject.has("textures")
+                    || !jsonObject.getAsJsonObject("textures").has("SKIN")
+                    || !jsonObject.getAsJsonObject("textures").getAsJsonObject("SKIN").has("url")) {
+                Bukkit.getLogger().log(Level.WARNING, "[AxGraves] Missing data in custom base64 skull texture when decoded-to JSON.");
+                return null;
+            }
+
+            String urlString = jsonObject.getAsJsonObject("textures").getAsJsonObject("SKIN").get("url").getAsString();
+            URL urlObject = URI.create(urlString).toURL();
+
+            PlayerProfile profile = Bukkit.createPlayerProfile(UUID.randomUUID());
+            PlayerTextures texturesProfile = profile.getTextures();
+            texturesProfile.setSkin(urlObject);
+            profile.setTextures(texturesProfile);
+
+            ItemStack head = new ItemStack(Material.PLAYER_HEAD);
+            if (head.getItemMeta() instanceof SkullMeta meta) {
+                meta.setOwnerProfile(profile);
+                head.setItemMeta(meta);
+                return head;
+            }
+
+            // if we reach here, it the item meta is not SkullMeta for some reason
+            Bukkit.getLogger().log(Level.WARNING, "[AxGraves] Failed to set custom skull texture.");
+            return null;
+
+        } catch (IllegalArgumentException e) {
+            Bukkit.getLogger().log(Level.WARNING, "[AxGraves] Invalid Base64 custom skull texture string provided.", e);
+            return null;
+        } catch (JsonSyntaxException e) {
+            Bukkit.getLogger().log(Level.WARNING, "[AxGraves] Invalid JSON in decoded custom skull texture data.", e);
+            return null;
+        } catch (Exception e) {
+            Bukkit.getLogger().log(Level.SEVERE, "[AxGraves] An unexpected error occurred while creating custom texture skull.", e);
+            return null;
+        }
     }
 }
